d37bf24c3d3d5a2355c2704242494edf6763b61b
xRodrigoSx <rodrgosantos123@hotmail.com>
2025-02-12 15:41:40 -0300

    ajustes na tela inicial

--- Angular/AngularVision/src/app/tela-inicial/tela-inicial.component.ts
+++ Angular/AngularVision/src/app/tela-inicial/tela-inicial.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { CompeticaoService } from '../services/competicao.service';
+
+interface Competicao {
+  nome: string;
+}
 
 @Component({
   selector: 'app-tela-inicial',
@@ -7,9 +12,49 @@ import { Router } from '@angular/router';
   styleUrls: ['./tela-inicial.component.css']
 })
 export class TelaInicialComponent implements OnInit {
+  competicoes: any[] = [];
+  filtroSelecionado: string = 'municipal';
+  usuarioLogado: any
 
-  constructor(private router: Router) { }
+  constructor(private userService: UserService,
+    private competicaoService: CompeticaoService
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getUsuarioLogado().subscribe(usuario => {
+      this.usuarioLogado = usuario;
+      this.buscarMinhasCompeticoes();
+    });
+  }
+
+  buscarMinhasCompeticoes(): void {
+    const params: { pais?: string,
+      estado?: string,
+      cidade?: string
+     } = {};
+    switch (this.filtroSelecionado) {
+      case 'mundial':
+      // No filter needed for mundial
+      break;
+      case 'nacional':
+      params.pais = this.usuarioLogado.pais;
+      break;
+      case 'estadual':
+      params.pais = this.usuarioLogado.pais;
+      params.estado = this.usuarioLogado.estado;
+      break;
+      case 'municipal':
+      params.pais = this.usuarioLogado.pais;
+      params.estado = this.usuarioLogado.estado;
+      params.cidade = this.usuarioLogado.cidade;
+      break;
+      default:
+      // Handle default case if needed
+      break;
+    }
+
+    this.competicaoService.buscarCompeticoes(params).subscribe(competicoes => {
+      this.competicoes = competicoes;
+    }, error => console.log("Erro ao buscar competições: ", error));
   }
 }
