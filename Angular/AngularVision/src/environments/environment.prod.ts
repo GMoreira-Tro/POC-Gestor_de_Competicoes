6a488f346a2d90ae2af01a36117566634a827cd4
xRodrigoSx <rodrgosantos123@hotmail.com>
2025-04-25 15:52:38 -0300

    prod.ts

--- Angular/AngularVision/src/environments/environment.prod.ts
+++ Angular/AngularVision/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: process.env['NG_ENV_API_URL'] || 'https://conectacompeticao-proud-mountain-2821.fly.dev'
+    apiBaseUrl: 'https://conectacompeticao-proud-mountain-2821.fly.dev'
   };
   
